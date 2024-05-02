dd2ccc900d7fd9f0c698f3bdd27946fa2bbf35e9
Kunal Khairnar <kkhairnar@nexuscognitive.com>
2024-05-02 19:48:27 +0530

    Initial admin- template changes

--- src/routes.js
+++ src/routes.js
@@ -21,14 +21,14 @@ import SignInCentered from "views/auth/signIn";
 
 const routes = [
   {
-    name: "Application",
+    name: "Main Dashboard",
     layout: "/admin",
     path: "/default",
     icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
     component: MainDashboard,
   },
   {
-    name: "Connections",
+    name: "NFT Marketplace",
     layout: "/admin",
     path: "/nft-marketplace",
     icon: (
@@ -43,19 +43,33 @@ const routes = [
     secondary: true,
   },
   {
-    name: "Cost",
+    name: "Data Tables",
     layout: "/admin",
     icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
     path: "/data-tables",
     component: DataTables,
   },
   {
-    name: "Security",
+    name: "Profile",
     layout: "/admin",
     path: "/profile",
     icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
     component: Profile,
-  }
+  },
+  {
+    name: "Sign In",
+    layout: "/auth",
+    path: "/sign-in",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: SignInCentered,
+  },
+  {
+    name: "RTL Admin",
+    layout: "/rtl",
+    path: "/rtl-default",
+    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    component: RTL,
+  },
 ];
 
 export default routes;
